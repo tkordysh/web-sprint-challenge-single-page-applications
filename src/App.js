b6922d90d7616a5023149e71853d5293156f8124
Tony Kordysh <tkordysh@gmail.com>
2022-04-01 15:06:24 -0500

    built home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import './App.css';
+import React, { useState, useEffect } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import OrderForm from './components/OrderForm';
+
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>My code</p>
-    </>
+    <div className='App'>
+      <header>
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/pizza">Pizza</Link>
+      </header>
+      <Switch>
+        <Route exact path="/">
+          <h1>Lambda Eats</h1>
+          <p>Welcome to the most delicious pizza place!</p>
+        </Route>
+        <Route exact path="/pizza">
+          <OrderForm />
+        </Route>
+      </Switch> 
+
+    </div>
+
   );
 };
 export default App;
+
+
---
+++ src/components/OrderForm.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import * as yup from 'yup';
+
+const OrderForm = (props) => {
+    return (
+        <article>
+            This is the order form
+        </article>
+    )
+}
+
+export default OrderForm
